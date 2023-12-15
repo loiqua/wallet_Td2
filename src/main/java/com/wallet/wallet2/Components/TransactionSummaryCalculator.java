7a830209336b09eeff6c7c7ce16e8a35fa46c833
Mampionona <hei.mampionona@gmail.com>
2023-12-15 16:11:20 +0300

    feat: add method getCategoryAmountsBetweenDates to calculate the sum of amounts per category between two dates for a specific account for TransactionSummaryCalculator.java

--- src/main/java/com/wallet/wallet2/Components/TransactionSummaryCalculator.java
+++ src/main/java/com/wallet/wallet2/Components/TransactionSummaryCalculator.java
@@ -7,7 +7,6 @@ package com.wallet.wallet2.Components;
 
     public class TransactionSummaryCalculator {
 
-        // Method to calculate the total amount between two dates for a specific account
         public static double getTotalAmountBetweenDates(int accountId, Date startDate, Date endDate) {
             double totalAmount = 0.0;
             Connection connection = ConnectionDB.getConnection();
@@ -28,5 +27,33 @@ package com.wallet.wallet2.Components;
             }
 
             return totalAmount;
+        }
+
+        // Method to calculate the sum of amounts per category between two dates for a specific account
+        public static Map<String, Double> getCategoryAmountsBetweenDates(int accountId, Date startDate, Date endDate) {
+            Map<String, Double> categoryAmounts = new HashMap<>();
+            Connection connection = ConnectionDB.getConnection();
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT COALESCE(c.category_name, 'No category') AS category, SUM(t.amount) AS total " +
+                            "FROM transactions t LEFT JOIN categories c ON t.category = c.category_name " +
+                            "WHERE t.account_id = ? AND t.dateTime BETWEEN ? AND ? " +
+                            "GROUP BY c.category_name")) {
+
+                preparedStatement.setInt(1, accountId);
+                preparedStatement.setDate(2, startDate);
+                preparedStatement.setDate(3, endDate);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    String category = resultSet.getString("category");
+                    double amount = resultSet.getDouble("total");
+                    categoryAmounts.put(category, amount);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            return categoryAmounts;
         }
     }
