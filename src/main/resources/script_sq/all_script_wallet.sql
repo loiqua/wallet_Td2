69ab885949e0604dbb38354e9774259805103bf1
Andriamahaly Loiqua <hei.loiqua@gmail.com>
2023-12-18 09:27:27 +0300

    modification table

--- src/main/resources/script_sq/all_script_wallet.sql
+++ src/main/resources/script_sq/all_script_wallet.sql
@@ -1,58 +1,76 @@
-CREATE DATABASE wallet;
+-- Création de la base de données s'il n'existe pas déjà
+CREATE DATABASE IF NOT EXISTS wallet_2;
 
+-- Sélection de la base de données
+\c wallet2;
 
-CREATE TABLE IF NOT EXISTS currency ( currency_id BIGSERIAL PRIMARY KEY,
-                                                                    name VARCHAR(100) NOT NULL,
-                                                                                      code VARCHAR(255) NOT NULL UNIQUE);
+-- Création de la table currency
+CREATE TABLE IF NOT EXISTS currency (
+                                        currency_id BIGSERIAL PRIMARY KEY,
+                                        name VARCHAR(100) NOT NULL,
+    code VARCHAR(255) NOT NULL UNIQUE
+    );
 
+-- Création de la table account
+CREATE TABLE IF NOT EXISTS account (
+                                       account_id BIGSERIAL PRIMARY KEY,
+                                       name VARCHAR(255) NOT NULL,
+    current_amount DOUBLE PRECISION DEFAULT 0,
+    account_type VARCHAR(255) NOT NULL,
+    account_number VARCHAR,
+    currency_id BIGINT REFERENCES currency(currency_id) ON DELETE CASCADE NOT NULL,
+    creation_timestamp TIMESTAMP DEFAULT NOW()
+    );
 
-CREATE TABLE IF NOT EXISTS account
-    ( account_id BIGSERIAL PRIMARY KEY,
-                                   name VARCHAR(255) NOT NULL,
-                                                     current_amount DOUBLE PRECISION DEFAULT 0,
-                                                                                             account_type VARCHAR(255) NOT NULL,
-                                                                                                                       account_number VARCHAR, currency_id BIGINT REFERENCES currency(currency_id) ON DELETE CASCADE NOT NULL,
-                                                                                                                                                                                                                     creation_timestamp TIMESTAMP DEFAULT NOW());
+-- Création de la table balance
+CREATE TABLE IF NOT EXISTS balance (
+                                       balance_id BIGSERIAL PRIMARY KEY,
+                                       balance_timestamp TIMESTAMP NOT NULL,
+                                       account_id BIGINT REFERENCES account(account_id) ON DELETE CASCADE NOT NULL,
+    amount DOUBLE PRECISION NOT NULL
+    );
 
+-- Création de la table transaction_tag
+CREATE TABLE IF NOT EXISTS transaction_tag (
+                                               tag_id SERIAL PRIMARY KEY,
+                                               name TEXT UNIQUE NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS balance
-    ( balance_id BIGSERIAL PRIMARY KEY,
-                                   balance_timestamp TIMESTAMP NOT NULL,
-                                                               account_id BIGINT REFERENCES account(account_id) ON DELETE CASCADE NOT NULL,
-                                                                                                                                  amount DOUBLE PRECISION NOT NULL);
+-- Création de la table transaction_category
+CREATE TABLE IF NOT EXISTS transaction_category (
+                                                    category_id SERIAL PRIMARY KEY,
+                                                    name TEXT UNIQUE NOT NULL
+);
 
+-- Création de la table transaction
+CREATE TABLE IF NOT EXISTS transaction (
+                                           transaction_id BIGSERIAL PRIMARY KEY,
+                                           tag_id INT REFERENCES transaction_tag(tag_id) ON DELETE CASCADE NOT NULL,
+    category_id INT REFERENCES transaction_category(category_id) ON DELETE SET NULL,
+    amount DOUBLE PRECISION DEFAULT 0,
+    transaction_type VARCHAR(255) NOT NULL,
+    transfer_to BIGINT REFERENCES transaction(transaction_id) ON DELETE CASCADE,
+    account_id BIGINT REFERENCES account(account_id) ON DELETE CASCADE,
+    creation_timestamp TIMESTAMP DEFAULT NOW()
+    );
 
-CREATE TABLE IF NOT EXISTS transaction_tag ( tag_id SERIAL PRIMARY KEY,
-                                                                   name TEXT UNIQUE NOT NULL);
+-- Création de la table transfer_history
+CREATE TABLE IF NOT EXISTS transfer_history (
+                                                transfer_history_id BIGSERIAL PRIMARY KEY,
+                                                debit_transaction_id BIGINT REFERENCES transaction(transaction_id) ON DELETE CASCADE,
+    credit_transaction_id BIGINT REFERENCES transaction(transaction_id) ON DELETE CASCADE,
+    transfer_date TIMESTAMP DEFAULT NOW()
+    );
 
+-- Création de la table currency_value
+CREATE TABLE IF NOT EXISTS currency_value (
+                                              currency_value_id BIGSERIAL PRIMARY KEY,
+                                              source_currency_id BIGINT REFERENCES currency(currency_id) ON DELETE CASCADE NOT NULL,
+    destination_currency_id BIGINT REFERENCES currency(currency_id) ON DELETE CASCADE NOT NULL,
+    change_rate DOUBLE PRECISION DEFAULT 0,
+    currency_value_date TIMESTAMP DEFAULT NOW()
+    );
 
-CREATE TABLE IF NOT EXISTS transaction_category ( category_id SERIAL PRIMARY KEY,
-                                                                             name TEXT UNIQUE NOT NULL);
-
-
-CREATE TABLE IF NOT EXISTS transaction
-    ( transaction_id BIGSERIAL PRIMARY KEY,
-                                       tag_id INT REFERENCES transaction_tag(tag_id) ON DELETE CASCADE NOT NULL,
-                                                                                                       category_id INT REFERENCES transaction_category(category_id) ON DELETE
-     SET NULL,
-         amount DOUBLE PRECISION DEFAULT 0,
-                                         transaction_type VARCHAR(255) NOT NULL,
-                                                                       transfer_to BIGINT REFERENCES transaction(transaction_id) ON DELETE CASCADE,
-                                                                                                                                           account_id BIGINT REFERENCES account(account_id) ON DELETE CASCADE,
-                                                                                                                                                                                                      creation_timestamp TIMESTAMP DEFAULT NOW());
-
-
-CREATE TABLE IF NOT EXISTS transfer_history
-    ( transfer_history_id BIGSERIAL PRIMARY KEY,
-                                            debit_transaction_id BIGINT REFERENCES transaction(transaction_id) ON DELETE CASCADE,
-                                                                                                                         credit_transaction_id BIGINT REFERENCES transaction(transaction_id) ON DELETE CASCADE,
-                                                                                                                                                                                                       transfer_date TIMESTAMP DEFAULT NOW());
-
-
-CREATE TABLE IF NOT EXISTS currency_value
-    ( currency_value_id BIGSERIAL PRIMARY KEY,
-                                          source_currency_id BIGINT REFERENCES currency(currency_id) ON DELETE CASCADE NOT NULL,
-                                                                                                                       destination_currency_id BIGINT REFERENCES currency(currency_id) ON DELETE CASCADE NOT NULL,
                                                                                                                                                                                                          change_rate DOUBLE PRECISION DEFAULT 0,
                                                                                                                                                                                                                                               currency_value_date TIMESTAMP DEFAULT NOW());
 
--- src/main/resources/script_sq/inserrt.sql
+++ src/main/resources/script_sq/inserrt.sql
@@ -1,79 +1,43 @@
--- Insertion de devises
+-- Inserting data into the currency table
+INSERT INTO currency (name, code) VALUES
+                                      ('US Dollar', 'USD'),
+                                      ('Euro', 'EUR'),
+                                      ('British Pound', 'GBP');
 
-INSERT INTO currency (name, code)
-VALUES ('Euro',
-        'EUR'), ('Dollar',
-                 'USD'), ('Yen',
-                          'JPY');
+-- Inserting data into the account table
+INSERT INTO account (name, current_amount, account_type, account_number, currency_id) VALUES
+                                                                                          ('Savings Account', 5000.00, 'Savings', '123456789', 1),
+                                                                                          ('Checking Account', 1500.00, 'Checking', '987654321', 2);
 
--- Insertion de comptes
+-- Inserting data into the balance table
+INSERT INTO balance (balance_timestamp, account_id, amount) VALUES
+                                                                (CURRENT_TIMESTAMP, 1, 5000.00),
+                                                                (CURRENT_TIMESTAMP, 2, 1500.00);
 
-INSERT INTO account (name, current_amount, account_type, account_number, currency_id)
-VALUES ('Compte Courant',
-        5000,
-        'Checking',
-        '123456789',
-        1), ('Compte Épargne',
-             10000,
-             'Savings',
-             '987654321',
-             2);
+-- Inserting data into the transaction_tag table
+INSERT INTO transaction_tag (name) VALUES
+                                       ('Groceries'),
+                                       ('Utilities'),
+                                       ('Entertainment');
 
--- Insertion d'étiquettes de transaction
+-- Inserting data into the transaction_category table
+INSERT INTO transaction_category (name) VALUES
+                                            ('Income'),
+                                            ('Expense');
 
-INSERT INTO transaction_tag (name)
-VALUES ('Foods'), ('Electronics'), ('Salaries');
+-- Inserting data into the transaction table
+INSERT INTO transaction (tag_id, category_id, amount, transaction_type, transfer_to, account_id) VALUES
+                                                                                                     (1, 2, 50.00, 'Expense', NULL, 1),
+                                                                                                     (2, 1, 2000.00, 'Income', NULL, 2),
+                                                                                                     (3, NULL, 100.00, 'Expense', NULL, 1);
 
--- Insertion de catégories de transaction
+-- Inserting data into the transfer_history table
+INSERT INTO transfer_history (debit_transaction_id, credit_transaction_id, transfer_date) VALUES
+                                                                                              (1, 2, CURRENT_TIMESTAMP),
+                                                                                              (3, NULL, CURRENT_TIMESTAMP);
 
-INSERT INTO transaction_category (name)
-VALUES ('Groceries'), ('Electronics'), ('Salary');
-
--- Insertion de transactions
-
-INSERT INTO transaction (tag_id,
-                         category_id,
-                         amount,
-                         transaction_type,
-                         transfer_to,
-                         account_id)
-VALUES (1,
-        1,
-        50,
-        'Spending',
-        NULL,
-        1), (2,
-             2,
-             100,
-             'Spending',
-             NULL,
-             1), (3,
-                  3,
-                  2000,
-                  'Income',
-                  NULL,
-                  2);
-
--- Insertion d'historique de transfert
-
-INSERT INTO transfer_history (debit_transaction_id, credit_transaction_id)
-VALUES (1,
-        2);
-
--- Insertion de la valeur des devises
-
-INSERT INTO currency_value (source_currency_id, destination_currency_id, change_rate)
-VALUES (1,
-        2,
-        1.2), (1,
-               3,
-               129.5), (2,
-                        1,
-                        0.8), (2,
-                               3,
-                               109.5), (3,
-                                        1,
-                                        0.0077), (3,
-                                                  2,
-                                                  0.0091);
+-- Inserting data into the currency_value table
+INSERT INTO currency_value (source_currency_id, destination_currency_id, change_rate, currency_value_date) VALUES
+                                                                                                               (1, 2, 0.85, CURRENT_TIMESTAMP),
+                                                                                                               (2, 1, 1.18, CURRENT_TIMESTAMP);
 
